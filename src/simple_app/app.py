fc50ed7471b951e81ba9afac6ce6ad8e1d24a64e
DrNattapoom <nattapoom.dr@gmail.com>
2023-03-24 17:18:21 +0700

    Hello World!

--- src/simple_app/app.py
+++ src/simple_app/app.py
@@ -23,7 +23,7 @@ migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
-    return "hello world!"
+    return "Hello World!"
 
 
 app.add_url_rule("/todo", methods=["GET"], view_func=todo_api.list_todo)
